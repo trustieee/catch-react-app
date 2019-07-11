8a246272e9ae5cc09c00364d68a5118ff9c611c5
mariocatch <mcacciatore@edatadimensions.com>
2019-07-11 12:53:51 -0400

    Extend express w/ friendlier uri prefix on gets. Add dotenv usage to express

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+require('dotenv').config();
+
+const id = process.env.MY_ENV_ID;
+const key = process.env.MY_ENV_KEY;
+
 const app = express();
 const port = 3001;
 const basePrefix = '/api/';
 
-const friendlyUri = uri => basePrefix + uri;
+app._get = (uri, ...props) => {
+  app.get(basePrefix + uri, props);
+};
 
 const foos = [{ name: 'foo' }, { name: 'bar' }];
 
@@ -12,6 +19,7 @@ app.get('*', (req, res, next) => {
   next();
 });
 
-app.get(friendlyUri('foos'), (req, res) => res.send(foos));
+app._get('foos', (req, res) => res.send(foos));
+app._get('env', (req, res) => res.send({ id, key }));
 
 app.listen(port, () => console.log(`Server running on :${port}!`));
